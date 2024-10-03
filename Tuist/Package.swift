d9611a21c67d9ad7c57175f3718fc1e77c7db863
namudi <namudongs@gmail.com>
2024-10-04 00:05:58 +0900

    Chore/#3: Xcode 및 Tuist 초기 설정
    라이브러리 추가 - RxSwift, SnapKit, ReactorKit, Hero, Realm

---
+++ Tuist/Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+#if TUIST
+    import ProjectDescription
+
+    let packageSettings = PackageSettings(
+        // Customize the product types for specific package product
+        // Default is .staticFramework
+        // productTypes: ["Alamofire": .framework,] 
+        productTypes: [:]
+    )
+#endif
+
+let package = Package(
+    name: "Haruby-iOS",
+    dependencies: [
+        // Add your own dependencies here:
+        // .package(url: "https://github.com/Alamofire/Alamofire", from: "5.0.0"),
+        // You can read more about dependencies here: https://docs.tuist.io/documentation/tuist/dependencies
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.7.1")),
+        .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.7.1")),
+        .package(url: "https://github.com/ReactorKit/ReactorKit.git", .upToNextMajor(from: "3.2.0")),
+        .package(url: "https://github.com/HeroTransitions/Hero.git", .upToNextMajor(from: "1.6.3")),
+        .package(url: "https://github.com/realm/realm-swift.git", .upToNextMajor(from: "10.54.0"))
+    ]
+)
