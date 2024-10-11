e32de8e88952b746fd40a8cfe19aee47bfc9e43c
Jeong Dong <eejxdong99@gmail.com>
2024-10-11 14:43:35 +0900

    Feature/#8: RealmStorage, SalaryBudgetRepository 정의

--- Haruby-iOS/Sources/Repository/SalaryBudgetRepository.swift
+++ Haruby-iOS/Sources/Repository/SalaryBudgetRepository.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+import RxSwift
+
+
+protocol SalaryBudgetRepository: AnyObject {
+    func create(_ salaryBudget: SalaryBudget) -> Observable<Void>
+    func fetch() -> Observable<[SalaryBudget]>
+    func update(_ salaryBudget: SalaryBudget) -> Observable<Void>
+    func delete(_ salaryBudget: SalaryBudget) -> Observable<Void>
+}
