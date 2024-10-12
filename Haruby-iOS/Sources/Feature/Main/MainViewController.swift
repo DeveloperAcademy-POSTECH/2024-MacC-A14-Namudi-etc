b5349cec017983acfc5c3530aa708dd6dd945dd2
namudi <namudongs@gmail.com>
2024-10-13 00:09:12 +0900

    Feature/#24: ReceiptView 구현

--- Haruby-iOS/Sources/Feature/Main/MainViewController.swift
+++ Haruby-iOS/Sources/Feature/Main/MainViewController.swift
@@ -10,48 +10,24 @@ import SnapKit
 
 final class MainViewController: UIViewController {
     
-    private let helloLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Hello, Namudi.etc!"
-        label.font = .pretendardSemibold_32
-        return label
-    }()
-    
-    private let helloLabelKR: UILabel = {
-        let label = UILabel()
-        label.text = "안녕하세요, 나무디와나머디!"
-        label.font = .pretendard(size: 24, weight: .thin)
-        return label
-    }()
-    
-    private let harubyImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = HarubyIOSImages.Image.purpleHaruby
-        imageView.contentMode = .scaleAspectFit
-        return imageView
-    }()
+    let receiptView = ReceiptView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .Haruby.green
-        view.addSubview(harubyImageView)
-        view.addSubview(helloLabel)
-        view.addSubview(helloLabelKR)
-        
-        harubyImageView.snp.makeConstraints { make in
-            make.bottom.equalTo(helloLabel.snp.top)
-            make.centerX.equalToSuperview()
-            make.width.equalTo(55)
-            make.height.equalTo(50)
-        }
-        
-        helloLabel.snp.makeConstraints { make in
-            make.center.equalToSuperview()
-        }
-        
-        helloLabelKR.snp.makeConstraints { make in
-            make.top.equalTo(helloLabel.snp.bottom).offset(8)
-            make.centerX.equalToSuperview()
+        setupView()
+        setupConstraints()
+    }
+    
+    private func setupView() {
+        view.backgroundColor = .systemBackground
+        view.addSubview(receiptView)
+    }
+    
+    private func setupConstraints() {
+        receiptView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(142)
+            make.left.right.equalToSuperview().inset(16)
+            make.height.equalTo(331)
         }
     }
 }
