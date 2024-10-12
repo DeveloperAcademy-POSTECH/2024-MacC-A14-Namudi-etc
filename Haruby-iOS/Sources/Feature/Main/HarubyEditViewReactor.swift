8dbcaaacd5a8f818cc1c7024acfd8bea0e326d86
shinseungjae <mok05247@naver.com>
2024-10-12 18:01:32 +0900

    Feature/#26: HarubyEditViewController & Reactor 추가

---
+++ Haruby-iOS/Sources/Feature/Main/HarubyEditViewReactor.swift
@@ -0,0 +1,35 @@
+//
+//  HarubyEditViewReactor.swift
+//  Haruby-iOS
+//
+//  Created by 신승재 on 10/12/24.
+//
+
+import ReactorKit
+import RxSwift
+
+class HarubyEditViewReactor: Reactor {
+    enum Action {
+        
+    }
+    
+    enum Mutation {
+        
+    }
+    
+    struct State {
+        
+    }
+    
+    let initialState: State = State()
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        // code
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        // code
+    }
+    
+    // MARK: - Private Methods
+}
