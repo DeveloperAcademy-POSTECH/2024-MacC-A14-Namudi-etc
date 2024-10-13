01a8ad0715761d4cea703fb8ca2e5cdb51e470fc
shinseungjae <mok05247@naver.com>
2024-10-13 15:09:22 +0900

    Feature/#10: 한국식으로 돈표기하는 Int Extension 추가

--- Haruby-iOS/Sources/Utility/Extension/Int+.swift
+++ Haruby-iOS/Sources/Utility/Extension/Int+.swift
@@ -23,4 +23,13 @@ extension Int {
         let number = formatter.string(from: NSNumber(value: self)) ?? "NA"
         return number + "원"
     }
+
+    // 한국식 돈 표기
+    func toKoreanCurrencyFormat() -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        numberFormatter.locale = Locale(identifier: "ko_KR")
+        
+        return numberFormatter.string(from: NSNumber(value: self)) ?? "\(self)"
+    }
 }
