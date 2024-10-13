c26383fad93719852163f059e2eb2a6cb92a9e6b
shinseungjae <mok05247@naver.com>
2024-10-13 14:45:02 +0900

    Feature/#10: 코드 리팩토링

--- Haruby-iOS/Sources/Feature/Calendar/CalendarViewCellReactor.swift
+++ Haruby-iOS/Sources/Feature/Calendar/CalendarViewCellReactor.swift
@@ -10,7 +10,7 @@ import UIKit
 import ReactorKit
 import RxSwift
 
-class CalendarViewCellReactor: Reactor {
+final class CalendarViewCellReactor: Reactor {
     enum Action {
         
     }
@@ -20,45 +20,30 @@ class CalendarViewCellReactor: Reactor {
     }
     
     struct State {
-        var dayNumber: String         // 날짜 숫자 (문자열로 표현)
-        var isFirstDayOfMonth: Bool   // 월의 첫 날인지 여부
-        var monthName: String         // 월 이름 (월의 첫 날에만 표시)
-        var isVisible: Bool           // 셀이 표시 가능한지 여부
-        var isHighlighted: Bool       // 강조 표시 여부 (오늘 날짜 또는 선택된 날짜)
+        var dayNumber: String           // 날짜 숫자
+        var isVisible: Bool             // 셀이 표시 가능한지 여부
+        var isToday: Bool               // 오늘 날짜인지 여부
+        var haruby: String?             // 하루비
+        var memo: String?               // 메모
+        var expense: TransactionRecord? // 지출
+        var income: TransactionRecord?  // 수입
     }
     
     let initialState: State
     
     init(dayItem: DayItem) {
+        
         let calendar = Calendar.current
         
-        let dayNumber: String
-        if let date = dayItem.date {
-            let dayComponent = calendar.component(.day, from: date)
-            if dayComponent == 1 {
-                let monthComponent = calendar.component(.month, from: date)
-                dayNumber = "\(monthComponent)/\(dayComponent)"
-            } else {
-                dayNumber = "\(dayComponent)"
-            }
-        } else {
-            dayNumber = ""
-        }
-        
-        // 월의 첫 날인지 확인
-        let isFirstDayOfMonth = dayItem.date.map { calendar.component(.day, from: $0) == 1 } ?? false
-        
-        // 월 이름 설정 (월의 첫 날이고 날짜가 있는 경우에만)
-        //let monthName = (isFirstDayOfMonth && dayItem.date != nil) ? dayItem.date!.toString(dateFormat: "MMM") : ""
-        let monthName = ""
+        let dayNumber = dayItem.date.map { "\(calendar.component(.day, from: $0))" } ?? ""
+        let monthNumber = dayItem.date.map { "\(calendar.component(.month, from: $0))" } ?? ""
 
-        // 초기 상태 설정
+        let isFirstDayOfMonth = dayItem.date.map { calendar.component(.day, from: $0) == 1 } ?? false
+
         initialState = State(
-            dayNumber: dayNumber,
-            isFirstDayOfMonth: isFirstDayOfMonth,
-            monthName: monthName,
-            isVisible: dayItem.date != nil,       // 날짜가 있으면 표시 가능
-            isHighlighted: dayItem.isToday || dayItem.isSelected  // 오늘 날짜이거나 선택된 날짜면 강조
+            dayNumber: isFirstDayOfMonth ? "\(monthNumber)/\(dayNumber)" : dayNumber,
+            isVisible: dayItem.date != nil,
+            isToday: dayItem.isToday
         )
     }
     
