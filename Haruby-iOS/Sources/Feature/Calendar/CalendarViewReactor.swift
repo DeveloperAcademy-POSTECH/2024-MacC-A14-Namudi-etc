5fd827b1b8072cd9b67050c8e78c90011d6fcd39
shinseungjae <mok05247@naver.com>
2024-10-13 14:08:43 +0900

    Feature/#10: DayItem 구조체 변경

--- Haruby-iOS/Sources/Feature/Calendar/CalendarViewReactor.swift
+++ Haruby-iOS/Sources/Feature/Calendar/CalendarViewReactor.swift
@@ -47,18 +47,16 @@ class CalendarViewReactor: Reactor {
         let calendar = Calendar.current
         let currentDate = Date()
         
-        // 현재 월을 기준으로 전후 12개월의 데이터 생성
-        return (-12...12).map { monthOffset in
+        return (-1...12).map { monthOffset in
             guard let monthDate = calendar.date(byAdding: .month, value: monthOffset, to: currentDate) else {
                 fatalError("Failed to create date")
             }
             
             let days = daysInMonth(monthDate)
-            return MonthSection(month: monthDate, items: days)
+            return MonthSection(firstDayOfMonth: monthDate, items: days)
         }
     }
     
-    // 특정 월의 일별 데이터를 생성하는 메서드
     private func daysInMonth(_ date: Date) -> [DayItem] {
         let calendar = Calendar.current
         let monthStart = calendar.date(from: calendar.dateComponents([.year, .month], from: date))!
@@ -70,21 +68,22 @@ class CalendarViewReactor: Reactor {
         
         var days: [DayItem] = []
         
-        // 월의 시작 요일 이전의 빈 날짜 추가
         for _ in 1..<firstWeekday {
-            days.append(DayItem(date: nil, isSelectable: false, isToday: false, isSelected: false, isCurrentMonth: false))
+            days.append(DayItem(date: nil, isInSalaryPeriod: false, isToday: false, isSelected: false, haruby: nil, memo: nil, expense: nil, income: nil))
         }
         
         let today = calendar.startOfDay(for: Date())
-        // 월의 각 날짜에 대한 DayItem 생성
         for day in 1...numberOfDaysInMonth {
             let date = calendar.date(byAdding: .day, value: day - 1, to: monthStart)!
             days.append(DayItem(
                 date: date,
-                isSelectable: true,
+                isInSalaryPeriod: true,
                 isToday: calendar.isDate(date, inSameDayAs: today),
                 isSelected: false,
-                isCurrentMonth: true
+                haruby: 12000,
+                memo: nil,
+                expense: nil,
+                income: nil
             ))
         }
         
@@ -94,7 +93,7 @@ class CalendarViewReactor: Reactor {
 
 
 struct MonthSection {
-    var month: Date // 해당 월의 첫날
+    var firstDayOfMonth: Date
     var items: [DayItem]
 }
 
@@ -106,9 +105,12 @@ extension MonthSection: SectionModelType {
 }
 
 struct DayItem {
-    let date: Date?
-    let isSelectable: Bool
-    let isToday: Bool
-    let isSelected: Bool
-    let isCurrentMonth: Bool
+    var date: Date?
+    var isInSalaryPeriod: Bool
+    var isToday: Bool
+    var isSelected: Bool
+    var haruby: Int?
+    var memo: String?
+    var expense: TransactionRecord?
+    var income: TransactionRecord?
 }
