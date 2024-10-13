32e34f01ec78fecc3d0f484df3b7a5d9495244eb
Seo-Jooyoung <tjwndud0412@naver.com>
2024-10-13 14:24:09 +0900

    Feature/#12: InputViewController 작업 전 MARK 주석 추가 및 세팅

---
+++ Haruby-iOS/Sources/Feature/Input/InputViewController.swift
@@ -0,0 +1,23 @@
+//
+//  InputViewController.swift
+//  Haruby-iOS
+//
+//  Created by Seo-Jooyoung on 10/10/24.
+//
+
+import UIKit
+import SnapKit
+
+class InputViewController: UIViewController {
+    // MARK: - Properties
+    
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: - UI Setup
+    
+}
