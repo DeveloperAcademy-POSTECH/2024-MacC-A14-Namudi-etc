850bc3e4caf13b5dfa6e226e9de2d2f85b64121a
Jeong Dong <eejxdong99@gmail.com>
2024-10-14 17:21:17 +0900

    Hotfix/#34: static var -> static let으로 변경

--- Haruby-iOS/Sources/Utility/Extension/Date+.swift
+++ Haruby-iOS/Sources/Utility/Extension/Date+.swift
@@ -10,15 +10,15 @@ import Foundation
 extension Date {
     /// Date 값이 어떤 날짜인지를 표현
     var formattedDate: Self {
-    var calendar = Calendar.current
-    
-    // 달력 표기 방법 설정
-    calendar.locale = .current
-    // 타임존(UTC 시간관련된 개념) 설정
-    calendar.timeZone = .current
-    
-    let dateComponent = calendar.dateComponents([.year, .month, .day], from: self)
-    
-    return calendar.date(from: dateComponent)!
+        var calendar = Calendar.current
+        
+        // 달력 표기 방법 설정
+        calendar.locale = .current
+        // 타임존(UTC 시간관련된 개념) 설정
+        calendar.timeZone = .current
+        
+        let dateComponent = calendar.dateComponents([.year, .month, .day], from: self)
+        
+        return calendar.date(from: dateComponent)!
     }
 }
