9cb8a86c4d4f420272ef9ac0493861c5ec3e7937
namudi <namudongs@gmail.com>
2024-10-15 00:53:52 +0900

    Feature/#23: AppDelegate & DIContainer 구현
    StubRepository Mock 데이터 생성

--- Haruby-iOS/Sources/App/DIContainer.swift
+++ Haruby-iOS/Sources/App/DIContainer.swift
@@ -8,6 +8,28 @@
 import Foundation
 
 final class DIContainer {
-    static let shared = DIContainer()
-    private init() {}
+  static let shared = DIContainer()
+  
+  private var dependencies: [String: Any] = [:]
+  
+  init() {
+    self.registerDependencies()
+  }
+  
+  private func register<T>(_ type: T.Type, dependency: T) {
+    let key = String(describing: type)
+    dependencies[key] = dependency
+  }
+  
+  func resolve<T>(_ type: T.Type) -> T? {
+      let key = String(describing: type)
+      return dependencies[key] as? T
+  }
+  
+  private func registerDependencies() {
+    self.register(
+        SalaryBudgetRepository.self,
+        dependency: StubSalaryBudgetRepository() // Stub 객체
+    )
+  }
 }
