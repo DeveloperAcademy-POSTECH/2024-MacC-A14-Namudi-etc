c2a8db1fcc330d03c872cc55ee1bb8479034258a
Seo-Jooyoung <tjwndud0412@naver.com>
2024-10-15 21:41:22 +0900

    Feature/#12: 날짜 선택하는 datePickerSheet 구현

---
+++ Haruby-iOS/Sources/Feature/Input/DatePickerSheet.swift
@@ -0,0 +1,50 @@
+//
+//  DatePickerSheet.swift
+//  Haruby-iOS
+//
+//  Created by Seo-Jooyoung on 10/15/24.
+//
+
+import UIKit
+import SnapKit
+
+class DatePickerSheet: UIViewController, UISheetPresentationControllerDelegate {
+    private lazy var datePicker: UIDatePicker = {
+        let picker = UIDatePicker()
+        picker.datePickerMode = .date
+        picker.preferredDatePickerStyle = .inline
+        picker.backgroundColor = .Haruby.white
+        return picker
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .Haruby.white
+        
+        setupSubviews()
+        setupConstraints()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if let sheet = self.sheetPresentationController {
+            sheet.delegate = self
+        }
+    }
+    
+    private func setupSubviews() {
+        self.view.addSubview(datePicker)
+    }
+    
+    private func setupConstraints() {
+        datePicker.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(15)
+            make.center.equalToSuperview()
+        }
+    }
+    
+    func presentationControllerDidAttemptToDismiss(_ presentationController: UIPresentationController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
