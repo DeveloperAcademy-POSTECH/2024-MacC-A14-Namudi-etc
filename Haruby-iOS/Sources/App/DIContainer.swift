d440c35b1cb5d7243dc48ae45491e9da79781be9
Jeong Dong <eejxdong99@gmail.com>
2024-10-17 15:14:54 +0900

    Feature/#45: DaiyBudgetRepository를 DIContainer에 등록

--- Haruby-iOS/Sources/App/DIContainer.swift
+++ Haruby-iOS/Sources/App/DIContainer.swift
@@ -8,28 +8,33 @@
 import Foundation
 
 final class DIContainer {
-  static let shared = DIContainer()
-  
-  private var dependencies: [String: Any] = [:]
-  
-  private init() {
-    self.registerDependencies()
-  }
-  
-  private func register<T>(_ type: T.Type, dependency: T) {
-    let key = String(describing: type)
-    dependencies[key] = dependency
-  }
-  
-  func resolve<T>(_ type: T.Type) -> T? {
-      let key = String(describing: type)
-      return dependencies[key] as? T
-  }
-  
-  private func registerDependencies() {
-    self.register(
-        SalaryBudgetRepository.self,
-        dependency: StubSalaryBudgetRepository() // Stub 객체
-    )
-  }
+    static let shared = DIContainer()
+    
+    private var dependencies: [String: Any] = [:]
+    
+    private init() {
+        self.registerDependencies()
+    }
+    
+    private func register<T>(_ type: T.Type, dependency: T) {
+        let key = String(describing: type)
+        dependencies[key] = dependency
+    }
+    
+    func resolve<T>(_ type: T.Type) -> T? {
+        let key = String(describing: type)
+        return dependencies[key] as? T
+    }
+    
+    private func registerDependencies() {
+        self.register(
+            SalaryBudgetRepository.self,
+            dependency: StubSalaryBudgetRepository() // Stub 객체
+        )
+        
+        self.register(
+            DailyBudgetRepository.self,
+            dependency: StubDailyBudgetRepository()
+        )
+    }
 }
