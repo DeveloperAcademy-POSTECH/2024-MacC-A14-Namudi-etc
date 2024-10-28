32dfda7b217d07c269dbda1750fd25fac8b78614
shinseungjae <mok05247@naver.com>
2024-10-29 00:52:36 +0900

    Feature/#57: 뷰 계층 구조 분리 및 배치 완료

--- Harubee-iOS/Sources/Presentation/Today/TodayView.swift
+++ Harubee-iOS/Sources/Presentation/Today/TodayView.swift
@@ -12,8 +12,12 @@ import SwiftUI
 struct TodayView: View {
   var body: some View {
     NavigationStack {
-      ZStack(alignment: .top) {
+      ZStack {
         Color.blue.ignoresSafeArea()
+        
+        TodayPrimaryLayerView()
+        
+        TodaySecondaryLayerView()
         
       }
       .toolbar {
@@ -31,6 +35,95 @@ struct TodayView: View {
     }
   }
 }
+
+// MARK: - TodayPrimaryLayerView
+private struct TodayPrimaryLayerView: View {
+  var body: some View {
+    Text("This is Today Primary Layer View")
+      .foregroundStyle(.white)
+  }
+}
+
+// MARK: - TodaySecondaryLayerView
+private struct TodaySecondaryLayerView: View {
+  var body: some View {
+    VStack {
+      
+      TodayHeaderView()
+      
+      Spacer()
+      
+      TodayFooterView()
+      
+    }.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+  }
+}
+
+// MARK: - TodayHeaderView
+private struct TodayHeaderView: View {
+  var body: some View {
+    HStack {
+      Text("2024년 10월 8일 (화)")
+        .bold()
+        .foregroundStyle(.white)
+    }.frame(maxWidth: .infinity, alignment: .trailing)
+      .padding(EdgeInsets(top: 6, leading: 0, bottom: 0, trailing: 16))
+  }
+}
+
+// MARK: - TodayFooterView
+private struct TodayFooterView: View {
+  var body: some View {
+    VStack(spacing: 0) {
+      CalendarStreakView()
+      
+      Button(action: {
+        print("실제 지출 입력하기 버튼 Tapped")
+      }, label: {
+        Text("실제 지출 및 수입 입력하기")
+          .bold()
+          .foregroundColor(.white)
+          .frame(maxWidth: .infinity, maxHeight: 56)
+          .background(Color.blue)
+          .cornerRadius(10)
+      })
+      .padding(.top, 17)
+      .frame(maxWidth: .infinity, minHeight: 50)
+    }
+    .frame(maxWidth: .infinity, maxHeight: 182, alignment: .top)
+    .padding(.horizontal, 18)
+    .background(.white)
+  }
+}
+
+// MARK: - CalendarStreakView
+private struct CalendarStreakView: View {
+  var body: some View {
+    Button(action: {
+      print("Calendar View로 이동")
+    }, label: {
+      VStack {
+        HStack {
+          Text("캘린더")
+          Spacer()
+          Image(systemName: "chevron.right")
+            .resizable()
+            .frame(width: 8, height: 12)
+        }
+        
+        HStack {
+          RoundedRectangle(cornerRadius: 10)
+            .frame(maxWidth: .infinity, maxHeight: 65)
+          RoundedRectangle(cornerRadius: 10)
+            .frame(maxWidth: 44, maxHeight: 65)
+          RoundedRectangle(cornerRadius: 10)
+            .frame(maxWidth: .infinity, maxHeight: 65)
+        }
+      }.padding(.top, 10)
+    })
+  }
+}
+
 
 #Preview {
   TodayView()
