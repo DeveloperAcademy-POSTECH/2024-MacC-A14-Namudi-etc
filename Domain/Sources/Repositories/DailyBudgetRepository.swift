9e042a7a99c1cb5dc84a180e577bdfd72dc3cea6
Jeong Dong <eejxdong99@gmail.com>
2024-10-28 21:05:29 +0900

    Feature/#60: Repository protocol 내 함수 문서화

--- Domain/Sources/Repositories/DailyBudgetRepository.swift
+++ Domain/Sources/Repositories/DailyBudgetRepository.swift
@@ -9,10 +9,33 @@
 import Foundation
 
 public protocol DailyBudgetRepository {
+  
+  /// DB에서 특정 날짜에 해당하는 DailyBudget을 가져옵니다.
+  /// - Parameter date: 날짜
+  /// - Returns: Optional(해당 날짜와 일치하느
   func readByDate(_ date: Date) async throws -> DailyBudget?
   
+  /// 하루비를 변경합니다.
+  /// - Parameters:
+  ///   - id: 변경할 DailyBudget의 ID
+  ///   - harubee: 변경할 하루비 금액
   func updateHarubee(_ id: String, harubee: Int) async throws
+  
+  /// 지출 금액을 변경합니다
+  /// - Parameters:
+  ///   - id: 변경할 DailyBudget의 ID
+  ///   - expense: 변경할 지출 금액
   func updateExpense(_ id: String, expense: Int) async throws
+  
+  /// 수입 금액을 변경합니다.
+  /// - Parameters:
+  ///   - id: 변경할 DailyBudget의 ID
+  ///   - income: 변경할 수입 금액
   func updateIncome(_ id: String, income: Int) async throws
+  
+  /// 메모 내역을 변경합니다.
+  /// - Parameters:
+  ///   - id: 변경할 DailyBudget의 ID
+  ///   - memo: 변경할 메모 내역
   func updateMemo(_ id: String, memo: [String]) async throws
 }
--- Domain/Sources/Repositories/SalaryBudgetRepository.swift
+++ Domain/Sources/Repositories/SalaryBudgetRepository.swift
@@ -9,15 +9,48 @@
 import Foundation
 
 public protocol SalaryBudgetRepository {
+  
+  /// SalaryBudget을 DB에 저장합니다.
+  /// - Parameter salaryBudget: 저장할 SalaryBudget
   func create(_ salaryBudget: SalaryBudget) async throws
   
+  
+  /// DB에 저장된 모든 SalaryBudget을 불러옵니다.
+  /// - Returns: DB에 저장된 모든 SalaryBudget
   func readAll() async throws -> [SalaryBudget]
+  
+  /// DB에서 특정 날짜에 해당하는 SalaryBudget을 가져옵니다.
+  /// - Parameter startDate: 시작 날짜
+  /// - Returns: Optional(시작 날짜에 해당하는 SalaryBudget)
   func readByStartDate(_ startDate: Date) async throws -> SalaryBudget?
   
+  
+  /// 총 고정 수입 금액을 변경합니다.
+  /// - Parameters:
+  ///   - id: 변경할 SalaryBudget의 ID
+  ///   - totalFixedIncome: 변경할 고정 수입 금액
   func updateTotalFixedIncome(_ id: String, totalFixedIncome: Int) async throws
+  
+  /// 고정 지출 내역을 변경합니다.
+  /// - Parameters:
+  ///   - id: 변경할 SalaryBudget의 ID
+  ///   - fixedExpenses: 변경할 고정 지출 내역
   func updateFixedExpenses(_ id: String, fixedExpenses: [TransactionItem]) async throws
+  
+  /// 잔액을 변경합니다.
+  /// - Parameters:
+  ///   - id: 변경할 SalaryBudget의 ID
+  ///   - balance: 변경할 잔액
   func updateBalance(_ id: String, balance: Int) async throws
+  
+  /// 기본 하루비를 변경합니다.
+  /// - Parameters:
+  ///   - id: 변경할 SalaryBudget의 ID
+  ///   - defaultHarubee: 변경할 기본 하루비 금액
   func updateDefaultHarubee(_ id: String, defaultHarubee: Double) async throws
   
+  
+  /// DB에서 SalaryBudget을 삭제합니다.
+  /// - Parameter id: 삭제할 SalaryBudget의 ID
   func deleteById(_ id: String) async throws
 }
