188e970ed5197947240a461e3e5d46a56a1ac155
Jeong Dong <eejxdong99@gmail.com>
2024-10-28 21:11:34 +0900

    Feature/#60: RepositoryImpl에 프로토콜 적용

---
+++ Data/Sources/Repositories/Impls/DailyBudgetRepositoryImpl.swift
@@ -0,0 +1,47 @@
+//
+//  DailyBudgetRepositoryImpl.swift
+//  Data
+//
+//  Created by 이정동 on 10/28/24.
+//  Copyright © 2024 namudiEtc. All rights reserved.
+//
+
+import Foundation
+import Domain
+import SwiftData
+
+public final class DailyBudgetRepositoryImpl: DailyBudgetRepository {
+  
+  private let modelContainer: ModelContainer
+  
+  public init(modelContainer: ModelContainer) {
+    self.modelContainer = modelContainer
+  }
+  
+  public func readByDate(_ date: Date) async throws -> Domain.DailyBudget? {
+    print("Impl: ", #function)
+    return nil
+  }
+  
+  public func updateHarubee(_ id: String, harubee: Int) async throws {
+    print("Impl: ", #function)
+    return
+  }
+  
+  public func updateExpense(_ id: String, expense: Int) async throws {
+    print("Impl: ", #function)
+    return
+  }
+  
+  public func updateIncome(_ id: String, income: Int) async throws {
+    print("Impl: ", #function)
+    return
+  }
+  
+  public func updateMemo(_ id: String, memo: [String]) async throws {
+    print("Impl: ", #function)
+    return
+  }
+  
+  
+}
---
+++ Data/Sources/Repositories/Impls/SalaryBudgetRepositoryImpl.swift
@@ -0,0 +1,62 @@
+//
+//  SalaryBudgetRepositoryImpl.swift
+//  Data
+//
+//  Created by 이정동 on 10/28/24.
+//  Copyright © 2024 namudiEtc. All rights reserved.
+//
+
+import Foundation
+import Domain
+import SwiftData
+
+public final class SalaryBudgetRepositoryImpl: SalaryBudgetRepository {
+  
+  private let modelContainer: ModelContainer
+  
+  public init(modelContainer: ModelContainer) {
+    self.modelContainer = modelContainer
+  }
+  
+  public func create(_ salaryBudget: Domain.SalaryBudget) async throws {
+    print("Impl: ", #function)
+    return
+  }
+  
+  public func readAll() async throws -> [Domain.SalaryBudget] {
+    print("Impl: ", #function)
+    return []
+  }
+  
+  public func readByStartDate(_ startDate: Date) async throws -> Domain.SalaryBudget? {
+    print("Impl: ", #function)
+    return nil
+  }
+  
+  public func updateTotalFixedIncome(_ id: String, totalFixedIncome: Int) async throws {
+    print("Impl: ", #function)
+    return
+  }
+  
+  public func updateFixedExpenses(_ id: String, fixedExpenses: [Domain.TransactionItem]) async throws {
+    print("Impl: ", #function)
+    return
+  }
+  
+  public func updateBalance(_ id: String, balance: Int) async throws {
+    print("Impl: ", #function)
+    return
+  }
+  
+  public func updateDefaultHarubee(_ id: String, defaultHarubee: Double) async throws {
+    print("Impl: ", #function)
+    return
+  }
+  
+  public func deleteById(_ id: String) async throws {
+    print("Impl: ", #function)
+    return
+  }
+  
+  
+}
