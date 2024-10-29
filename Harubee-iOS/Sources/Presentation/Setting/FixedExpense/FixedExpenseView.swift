5d2604952093f961a856c76f51ab969a2cbc4a46
Seo-Jooyoung <tjwndud0412@naver.com>
2024-10-29 15:00:56 +0900

    Feature/#58: FixedExpenseView 간단한 레이아웃만 구현

--- Harubee-iOS/Sources/Presentation/Setting/FixedExpense/FixedExpenseView.swift
+++ Harubee-iOS/Sources/Presentation/Setting/FixedExpense/FixedExpenseView.swift
@@ -10,7 +10,50 @@ import SwiftUI
 
 struct FixedExpenseView: View {
   var body: some View {
-    FixedExpenseInputCell(fixedExpenseName: "", fixedExpenseAmount: "")
+    ZStack {
+      Color.blue
+        .ignoresSafeArea()
+      VStack {
+        HeaderView()
+        BodyView()
+          .ignoresSafeArea()
+      }
+    }
+  }
+}
+
+private struct HeaderView: View {
+  var body: some View {
+    VStack(alignment: .leading, spacing: 6) {
+      Text("매월 나가는 지출 내역을")
+      Text("추가해 주세요")
+    }
+    .font(.system(size: 24, weight: .bold))
+    .foregroundStyle(Color.white)
+    .frame(maxWidth: .infinity, alignment: .leading)
+    .padding(.horizontal, 26)
+    .padding(.top, 32)
+  }
+}
+
+private struct BodyView: View {
+  var body: some View {
+    ZStack(alignment: .leading) {
+      Rectangle()
+        .fill(Color.white)
+        .clipShape(RoundedRectangle(cornerRadius: 20))
+      
+      VStack {
+        Text("고정지출 내역")
+          .padding(.top, 38)
+          .padding(.horizontal, 24)
+          .font(.system(size: 20, weight: .semibold))
+          .frame(maxWidth: .infinity, alignment: .leading)
+        FixedExpenseInputCell(fixedExpenseName: "", fixedExpenseAmount: "")
+        
+      }
+    }
+    .padding(.top, 27)
   }
 }
 
