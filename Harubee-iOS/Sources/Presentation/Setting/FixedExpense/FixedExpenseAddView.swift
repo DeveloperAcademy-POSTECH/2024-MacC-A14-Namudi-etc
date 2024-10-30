d141f751ac65bc7b8f85f6cf2d2cf177b5ce04a8
Seo-Jooyoung <tjwndud0412@naver.com>
2024-10-30 19:13:26 +0900

    Feature/#58: VStack으로 감쌀 필요가 있는 코드에 VStack 추가

--- Harubee-iOS/Sources/Presentation/Setting/FixedExpense/FixedExpenseAddView.swift
+++ Harubee-iOS/Sources/Presentation/Setting/FixedExpense/FixedExpenseAddView.swift
@@ -14,27 +14,29 @@ struct FixedExpenseAddView: View {
   @State var fixedExpenseAmount: String
   
   var body: some View {
-    Text("고정지출 내역 추가")
-      .font(.system(size: 18))
-      .padding(.top, 20)
-    
-    BodyView(fixedExpenseName: $fixedExpenseName, fixedExpenseAmount: $fixedExpenseAmount)
-      .padding(.top, 37)
-    
-    Spacer()
-    
-    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-      Text("저장하기")
+    VStack(spacing: 0) {
+      Text("고정지출 내역 추가")
         .font(.system(size: 18))
-        .foregroundStyle(.white)
-        .padding(.horizontal, 149)
-        .padding(.vertical, 20)
-        .background(
-          RoundedRectangle(cornerRadius: 10)
-            .fill(.purple)
-        )
-    })
+        .padding(.top, 20)
+      
+      BodyView(fixedExpenseName: $fixedExpenseName, fixedExpenseAmount: $fixedExpenseAmount)
+        .padding(.top, 37)
+      
+      Spacer()
+      
+      Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+        Text("저장하기")
+          .font(.system(size: 18))
+          .foregroundStyle(.white)
+          .padding(.horizontal, 149)
+          .padding(.vertical, 20)
+          .background(
+            RoundedRectangle(cornerRadius: 10)
+              .fill(.purple)
+          )
+      })
       .padding(.bottom, 9)
+    }
   }
 }
 
