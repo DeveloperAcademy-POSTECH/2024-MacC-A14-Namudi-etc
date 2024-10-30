1b9f7437563250cfecf78324b8665a78e015b645
Jeong Dong <eejxdong99@gmail.com>
2024-10-30 10:51:15 +0900

    Feature/#64: DailyBudgetRepositoryImpl 구현

--- Data/Sources/Repositories/Impls/DailyBudgetRepositoryImpl.swift
+++ Data/Sources/Repositories/Impls/DailyBudgetRepositoryImpl.swift
@@ -18,30 +18,55 @@ public final class DailyBudgetRepositoryImpl: DailyBudgetRepository {
     self.modelContext = modelContext
   }
   
-  public func readByDate(_ date: Date) throws -> Domain.DailyBudget? {
+  public func readByDate(_ date: Date) throws -> DailyBudget? {
     print("Impl:", #function)
-    return nil
+    
+    let predicate = #Predicate<DailyBudgetDTO> { $0.date == date }
+    let descriptor = FetchDescriptor(predicate: predicate)
+    do {
+      let data = try modelContext.fetch(descriptor)
+      return data.first?.toEntity()
+    } catch {
+      throw SwiftDataError.fetchError
+    }
   }
   
   public func updateHarubee(_ id: String, harubee: Int) throws {
     print("Impl:", #function)
-    return
+    
+    guard let model = try readById(id) else { return }
+    model.harubee = harubee
   }
   
   public func updateExpense(_ id: String, expense: Int) throws {
     print("Impl:", #function)
-    return
+    
+    guard let model = try readById(id) else { return }
+    model.expense = expense
   }
   
   public func updateIncome(_ id: String, income: Int) throws {
     print("Impl:", #function)
-    return
+    guard let model = try readById(id) else { return }
+    model.income = income
   }
   
   public func updateMemo(_ id: String, memo: [String]) throws {
     print("Impl:", #function)
-    return
+    guard let model = try readById(id) else { return }
+    model.memo = memo
   }
   
-  
+  private func readById(_ id: String) throws -> DailyBudgetDTO? {
+    print("Impl:", #function)
+    
+    let predicate = #Predicate<DailyBudgetDTO> { $0.identifier == id }
+    let descriptor = FetchDescriptor(predicate: predicate)
+    do {
+      let data = try modelContext.fetch(descriptor)
+      return data.first
+    } catch {
+      throw SwiftDataError.fetchError
+    }
+  }
 }
