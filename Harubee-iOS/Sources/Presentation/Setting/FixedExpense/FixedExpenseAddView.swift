912a31d484cb88f9f90804acdd92044c8162d9e9
Seo-Jooyoung <tjwndud0412@naver.com>
2024-10-30 16:42:46 +0900

    Feature/#58: CustomTextField의 애니메이션 추가

--- Harubee-iOS/Sources/Presentation/Setting/FixedExpense/FixedExpenseAddView.swift
+++ Harubee-iOS/Sources/Presentation/Setting/FixedExpense/FixedExpenseAddView.swift
@@ -95,16 +95,19 @@ private struct DayPickerButton: View {
 private struct CustomTextfield: View {
   var title: String
   @Binding var text: String
+  @State private var isFocused: Bool = false
   
   var body: some View {
     VStack(spacing: 0) {
       Text(title)
         .font(.system(size: 12))
         .frame(maxWidth: .infinity, alignment: .leading)
+        .foregroundStyle(isFocused || !text.isEmpty ? .black : .clear)
+        .offset(y: isFocused || !text.isEmpty ? -4 : 0)
+        .animation(.easeOut(duration: 0.2), value: isFocused || !text.isEmpty)
       TextField(title, text: $text)
         .frame(maxWidth: .infinity)
         .font(.system(size: 16))
-        .padding(.top, 4)
       Rectangle()
         .frame(height: 1)
         .foregroundStyle(Color.gray)
