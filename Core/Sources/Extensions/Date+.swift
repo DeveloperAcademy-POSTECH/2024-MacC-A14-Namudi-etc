d56990a24b71d89c7991249b27823bd1b4c3c59c
Jeong Dong <eejxdong99@gmail.com>
2024-10-31 16:25:36 +0900

    Feature/#77: String+, Date+ 추가

--- Core/Sources/Extensions/Date+.swift
+++ Core/Sources/Extensions/Date+.swift
@@ -9,11 +9,28 @@
 import Foundation
 
 public extension Date {
+  
+  private var configuredCalendar: Calendar {
+    var calendar = Calendar.current
+    // 달력 표기 방법 설정
+    calendar.locale = .current
+    // 타임존 설정
+    calendar.timeZone = .current
+    return calendar
+  }
+  
   /// 년도 월 일 (요일) 표기
   var koreanFullDateString: String {
     let formatter = DateFormatter()
     formatter.dateFormat = "yyyy년 MM월 dd일 (E)"
     formatter.locale = Locale(identifier: "ko_KR")
     return formatter.string(from: self)
+  }
+  
+  /// Date 값이 어떤 날짜인지를 표현
+  var formattedDate: Self {
+    let calendar = configuredCalendar
+    let dateComponent = calendar.dateComponents([.year, .month, .day], from: self)
+    return calendar.date(from: dateComponent)!
   }
 }
