02feda8f55b1efe42ab8540368d54082a5fa07f3
namudi <namudongs@gmail.com>
2024-10-31 20:18:34 +0900

    Feature/#59: 뷰모델 초기 구현

--- Harubee-iOS/Sources/Presentation/Calendar/CalendarViewModel.swift
+++ Harubee-iOS/Sources/Presentation/Calendar/CalendarViewModel.swift
@@ -6,9 +6,58 @@
 //  Copyright © 2024 namudiEtc. All rights reserved.
 //
 
+import Domain
 import Foundation
 
 @Observable
 final class CalendarViewModel {
+  struct State {
+    
+  }
   
+  enum Action {
+    case onAppear
+    case moveNextPeriod
+    case movePreviousPeriod
+    case onDateSelected(Date)
+    case onMoveToToday
+  }
+  
+  private enum Effect {
+    case setSalaryBudget(SalaryBudget)
+    case setError(Error)
+  }
+  
+  // MARK: - Properties
+  private(set) var state = State()
+  
+  
+  init() {}
+  
+  // MARK: - Public Methods
+  func send(_ action: Action) {
+    switch action {
+    case .onAppear:
+      print("onAppear")
+    case .moveNextPeriod:
+      print("moveNextPeriod")
+    case .movePreviousPeriod:
+      print("movePreviousPeriod")
+    case .onDateSelected(_):
+      print("onDateSelected")
+    case .onMoveToToday:
+      print("onMoveToToday")
+    }
+  }
+  
+  private func receive(_ effect: Effect) {
+    switch effect {
+    case .setSalaryBudget:
+      print("setSalaryBudget")
+    case .setError:
+      print("setError")
+    }
+  }
+  
+  // MARK: - Private Methods
 }
