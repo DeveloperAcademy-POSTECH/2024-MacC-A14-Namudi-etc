bf418a583729beec2d876da251f1a962f516732f
namudi <namudongs@gmail.com>
2024-10-31 18:48:14 +0900

    Feature/#59: 캘린더 디자인 적용

--- Harubee-iOS/Sources/App/HarubeeIOSApp.swift
+++ Harubee-iOS/Sources/App/HarubeeIOSApp.swift
@@ -14,12 +14,24 @@ struct HarubeeIOSApp: App {
   
   init() {
     Font.registerFont()
+    
+    let appearance = UINavigationBarAppearance()
+    appearance.configureWithOpaqueBackground()
+    
+    appearance.titleTextAttributes = [.foregroundColor: UIColor(Color.whiteDefault)]
+    appearance.largeTitleTextAttributes = [.foregroundColor: UIColor(Color.whiteDefault)]
+    appearance.backgroundColor = UIColor(Color.main)
+    appearance.shadowColor = .clear
+    
+    UINavigationBar.appearance().standardAppearance = appearance
+    UINavigationBar.appearance().scrollEdgeAppearance = appearance
   }
   
   var body: some Scene {
     WindowGroup {
-      TodayView()
-        .environment(TodayViewModel())
+      NavigationStack {
+        CalendarView()
+      }
     }
   }
 }
