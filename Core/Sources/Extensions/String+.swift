9ff3f0b702a5b314cfac4b088428fece3036b0d7
Jeong Dong <eejxdong99@gmail.com>
2024-10-31 16:25:36 +0900

    Feature/#77: String+, Date+ 추가

---
+++ Core/Sources/Extensions/String+.swift
@@ -0,0 +1,22 @@
+//
+//  String.swift
+//  Core
+//
+//  Created by 이정동 on 10/31/24.
+//  Copyright © 2024 namudiEtc. All rights reserved.
+//
+
+import Foundation
+
+public extension String {
+  subscript(index: Int) -> Self {
+    get {
+      let index = self.index(self.startIndex, offsetBy: index)
+      return String(self[index])
+    }
+  }
+  
+  var isSingleNumber: Bool {
+    ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"].contains(self)
+  }
+}
