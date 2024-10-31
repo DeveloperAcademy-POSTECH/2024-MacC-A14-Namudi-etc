130a714eb5a582e5df57fd95e9256ed792e6f071
Jeong Dong <eejxdong99@gmail.com>
2024-10-31 19:33:39 +0900

    Feature/#77: Core 모둘 Extension에 주석 추가

--- Core/Sources/Extensions/String+.swift
+++ Core/Sources/Extensions/String+.swift
@@ -16,10 +16,12 @@ public extension String {
     }
   }
   
+  /// 해당 문자열이 단일 숫자(한자리 수)인지를 판단합니다. - [Ex. 5 = true, 10 = false]
   var isSingleNumber: Bool {
     ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"].contains(self)
   }
   
+  /// decimal 포멧으로 된 문자열을 정수로 변환해줍니다. - [Ex. 12,300원 -> 12300]
   var numberFormat: Int? {
     let string = self
       .replacingOccurrences(of: ",", with: "")
