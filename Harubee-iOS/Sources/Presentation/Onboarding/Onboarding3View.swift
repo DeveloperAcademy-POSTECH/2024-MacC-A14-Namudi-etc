423801c4871d93e89b761745bf4a75df1db8ce00
Seo-Jooyoung <tjwndud0412@naver.com>
2024-11-02 02:14:46 +0900

    Feature/#90: 세 번째 온보딩 뷰 구현

--- Harubee-iOS/Sources/Presentation/Onboarding/Onboarding3View.swift
+++ Harubee-iOS/Sources/Presentation/Onboarding/Onboarding3View.swift
@@ -7,13 +7,106 @@
 //
 
 import SwiftUI
+import DesignSystem
 
 struct Onboarding3View: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  @State private var selectedDay: Int = 1
+  @State private var fixedIncomeAmount: String = ""
+  
+  var body: some View {
+    VStack(spacing: 0) {
+      OnboardingHeaderView()
+        .padding(.top, 83)
+        .padding(.bottom, 33)
+        .background(
+          Rectangle()
+            .fill(Color.main)
+        )
+      OnboardingBodyView(selectedDay: $selectedDay, fixedIncomeAmount: $fixedIncomeAmount)
+      
     }
+    .ignoresSafeArea()
+  }
+}
+
+private struct OnboardingHeaderView: View {
+  var body: some View {
+    VStack(spacing: 26) {
+      HStack(spacing: 0) {
+        Button {
+          print("뒤로가기")
+        } label: {
+          Image(systemName: "chevron.left")
+            .font(Font.system(size: 18, weight: .medium))
+            .foregroundStyle(Color.whiteDefault)
+        }
+        Text("1/3")
+          .font(.pretendardSemibold_22)
+          .foregroundStyle(Color.whiteDeep50)
+          .frame(maxWidth: .infinity, alignment: .trailing)
+      }
+      
+      VStack(alignment: .leading, spacing: 6) {
+        Text("먼저, 하루비를 계산하기 위한")
+        Text("기본 정보를 입력해주세요")
+      }
+      .frame(maxWidth: .infinity, alignment: .leading)
+      .font(.pretendardSemibold_24)
+      .foregroundStyle(Color.whiteDefault)
+    }
+    .padding(.horizontal, 20)
+  }
+}
+
+private struct OnboardingBodyView: View {
+  @Binding var selectedDay: Int
+  @Binding var fixedIncomeAmount: String
+  
+  var body: some View {
+    VStack(spacing: 30) {
+      DayPickerView(title: "주요 수입일은 언제인가요?",
+                    titleFont: .onboarding,
+                    selectedDay: $selectedDay
+      )
+      .padding(.top, 34)
+      
+      VStack(alignment: .leading, spacing: 0) {
+        Text("한 달의 수입금은 얼마인가요?")
+          .font(.pretendardMedium_20)
+          .foregroundStyle(Color.textBlack)
+        
+        Text("*입력하신 정보는 수입 기간동안의 하루비를 계산할 때만 사용됩니다")
+          .font(.pretendardMedium_12)
+          .foregroundStyle(Color.textBlack30)
+          .padding(.top, 6)
+        
+        FloatingTitleTextField(title: "금액", text: $fixedIncomeAmount)
+          .padding(.top, 24)
+      }
+      .padding(.horizontal, 20)
+      
+      Spacer()
+      
+      Button {
+        
+      } label: {
+        Text("저장하기")
+          .font(.pretendardSemibold_18)
+          .foregroundStyle(Color.whiteDeep)
+          .padding(.horizontal, 149)
+          .padding(.vertical, 20)
+          .background(
+            RoundedRectangle(cornerRadius: 10)
+              .fill(Color.main)
+            // textfield 다 안 채워지면 Main_30
+          )
+      }
+      .padding(.bottom, 43)
+    }
+    .frame(maxHeight: .infinity, alignment: .top)
+  }
 }
 
 #Preview {
-    Onboarding3View()
+  Onboarding3View()
 }
