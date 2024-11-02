a1eeb6e6f6be55af398844d6ac1936443b189f65
Seo-Jooyoung <tjwndud0412@naver.com>
2024-11-03 01:04:09 +0900

    Feature/#90: Onboarding2View 컴포넌트 삭제 및 화면 재구현

--- Harubee-iOS/Sources/Presentation/Onboarding/Onboarding2View.swift
+++ Harubee-iOS/Sources/Presentation/Onboarding/Onboarding2View.swift
@@ -15,15 +15,14 @@ struct Onboarding2View: View {
       Color.main.ignoresSafeArea()
       VStack(spacing: 0) {
         TitleView()
-        TextContentView(content1: "하루비는 다음 수입일까지",
-                        content2: "하루에 쓸 수 있는 금액을 미리 알려주는 앱",
-                        content3: "이에요"
-        )
+        
+        HarubeeExplainView()
+        .padding(.horizontal, 16)
         .padding(.top, 12)
-        TextContentView(content1: "하루비의 계산 방법은 아래와 같아요")
-          .padding(.top, 73)
+        
         CalculateContentView()
-          .padding(.top, 12)
+          .padding(.top, 73)
+          .padding(.horizontal, 16)
       }
       .frame(maxHeight: .infinity, alignment: .top)
       .padding(.top, 76)
@@ -51,76 +50,66 @@ private struct TitleView: View {
   }
 }
 
-private struct TextContentView: View {
-  private var content1: String
-  private var content2: String?
-  private var content3: String?
-  
-  init(content1: String,
-       content2: String? = nil,
-       content3: String? = nil
-  ) {
-    self.content1 = content1
-    self.content2 = content2
-    self.content3 = content3
-  }
-  
+private struct HarubeeExplainView: View {
   var body: some View {
     VStack(spacing: 22) {
       Rectangle()
         .frame(height: 1)
         .foregroundStyle(Color.textBrighter30)
-        .padding(.leading,
-                 content2 == nil || content3 == nil ? 4 : 0
-        )
-      
       VStack(alignment: .leading, spacing: 8) {
-        Text(content1)
+        Text("하루비는 다음 수입일까지")
           .font(.pretendardMedium_16)
           .foregroundStyle(Color.whiteDeep50)
           .padding(.leading, 4)
-        
-        if !(content2 == nil && content3 == nil) {
-          HStack(spacing: 3) {
-            ZStack(alignment: .bottom) {
-              Text(content2 ?? "")
-                .padding(.horizontal, 4)
-                .foregroundStyle(content2 == nil ? .clear : Color.whiteDefault)
-                .background(
-                  Rectangle()
-                  // TODO: 추가되는 색상으로 수정 필요
-                    .fill(content2 == nil ? .clear : Color.blue)
-                    .frame(height: 7)
-                    .offset(y: 8)
-                )
-            }
-            Text(content3 ?? "")
-              .foregroundStyle(content2 == nil || content3 == nil
-                               ? .clear
-                               : Color.whiteDeep50
+        HStack(spacing: 3) {
+          ZStack(alignment: .bottom) {
+            Text("하루에 쓸 수 있는 금액을 미리 알려주는 앱")
+              .padding(.horizontal, 4)
+              .foregroundStyle(Color.whiteDefault)
+              .background(
+                Rectangle()
+                // TODO: 추가되는 색상으로 수정 필요
+                  .fill(Color.blue)
+                  .frame(height: 7)
+                  .offset(y: 8)
               )
           }
+          Text("이에요")
+            .foregroundStyle(Color.whiteDeep50)
         }
       }
       .frame(maxWidth: .infinity, alignment: .leading)
     }
-    .padding(.horizontal, 16)
   }
 }
 
 private struct CalculateContentView: View {
   var body: some View {
-    Text("(나의 수입 - 고정지출) ÷ 다음 주요 수입일까지 남은 일수")
-      .font(.pretendardSemibold_14)
-      .foregroundStyle(Color.whiteDefault)
-      .frame(maxWidth: .infinity)
-      .padding(.vertical, 16)
-      .background(
-        RoundedRectangle(cornerRadius: 10)
-        // TODO: 추가된 색상으로 변경 필요
-          .fill(Color.black)
-          .padding(.horizontal, 16)
-      )
+    VStack(spacing: 0) {
+      Rectangle()
+        .frame(height: 1)
+        .foregroundStyle(Color.textBrighter30)
+        .padding(.horizontal, 4)
+      
+      Text("하루비의 계산 방법은 아래와 같아요")
+        .font(.pretendardMedium_16)
+        .foregroundStyle(Color.whiteDeep50)
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(.leading, 4)
+        .padding(.top, 22)
+      
+      Text("(나의 수입 - 고정지출) ÷ 다음 주요 수입일까지 남은 일수")
+        .font(.pretendardSemibold_14)
+        .foregroundStyle(Color.whiteDefault)
+        .frame(maxWidth: .infinity)
+        .padding(.vertical, 16)
+        .background(
+          RoundedRectangle(cornerRadius: 10)
+          // TODO: 추가된 색상으로 변경 필요
+            .fill(Color.black)
+        )
+        .padding(.top, 12)
+    }
   }
 }
 
