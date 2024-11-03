72e23f3e8794848b201cd6773c3c732628e17e74
shinseungjae <mok05247@naver.com>
2024-11-03 14:24:33 +0900

    Feature/#101: InfoBubble 모디파이어 추가

---
+++ Harubee-iOS/Sources/Presentation/Components/InfoBubble.swift
@@ -0,0 +1,116 @@
+//
+//  infoBubble.swift
+//  Harubee-iOS
+//
+//  Created by 신승재 on 10/30/24.
+//  Copyright © 2024 namudiEtc. All rights reserved.
+//
+
+import SwiftUI
+import DesignSystem
+
+struct InfoBubble<Label: View>: ViewModifier {
+  
+  @State private var contentSize: CGSize = .zero
+  @State private var labelSize: CGSize = .zero
+  
+  private var isVisible: Bool
+  private var alignment: Alignment
+  private var isOnBottom: Bool
+  private var label: () -> Label
+  
+  init(isVisible: Bool,
+       alignment: Alignment,
+       isOnBottom: Bool,
+       @ViewBuilder label: @escaping () -> Label) {
+    self.isVisible = isVisible
+    self.alignment = alignment
+    self.isOnBottom = isOnBottom
+    self.label = label
+  }
+  
+  func body(content: Content) -> some View {
+    if isVisible {
+      content
+        .background(
+          GeometryReader { contentProxy in
+            Color.clear.preference(key: ContentSizeKey.self,
+                                   value: CGSize(width: contentProxy.size.width,
+                                                 height: contentProxy.size.height))
+          }
+        )
+        .onPreferenceChange(ContentSizeKey.self) { newSize in
+          contentSize = newSize
+        }
+        .overlay(
+          VStack(spacing: 0) {
+            label()
+              .fixedSize()
+              .padding(10)
+              .background(
+                GeometryReader { labelProxy in
+                  Color.clear.preference(key: LabelSizeKey.self,
+                                         value: CGSize(width: labelProxy.size.width,
+                                                       height: labelProxy.size.height))
+                }
+              )
+              .onPreferenceChange(LabelSizeKey.self) { newSize in
+                labelSize = newSize
+              }
+          }
+          .background(Color.whiteDefault)
+          .cornerRadius(8)
+          .offset(calculateOffset())
+        )
+        .overlay(
+          Image(systemName: "triangle.fill")
+            .resizable()
+            .frame(width: 20, height: 14)
+            .rotationEffect(.degrees(isOnBottom ? 0 : 180))
+            .foregroundStyle(Color.whiteDefault)
+            .offset(y: isOnBottom ? (contentSize.height + 20) / 2 : -(contentSize.height + 20) / 2)
+        )
+    } else { content }
+  }
+  
+  private func calculateOffset() -> CGSize {
+    let xOffset: CGFloat
+    let yOffset: CGFloat
+    
+    switch alignment {
+    case .leading:
+      xOffset = -(contentSize.width - labelSize.width) / 2
+    case .trailing:
+      xOffset = (contentSize.width - labelSize.width) / 2
+    default:
+      xOffset = 0
+    }
+    
+    yOffset = isOnBottom ? (contentSize.height + labelSize.height + 20) / 2 : -(labelSize.height + contentSize.height + 20) / 2
+    
+    return CGSize(width: xOffset, height: yOffset)
+  }
+}
+
+extension View {
+  public func infoBubble<Label: View>(isVisible: Bool,
+                                      alignment: Alignment = .center,
+                                      isOnBottom: Bool = false,
+                                      @ViewBuilder label: @escaping () -> Label) -> some View {
+    modifier(InfoBubble(isVisible: isVisible, alignment: alignment, isOnBottom: isOnBottom, label: label))
+  }
+}
+
+struct LabelSizeKey: PreferenceKey {
+  static var defaultValue: CGSize { .zero }
+  static func reduce(value: inout CGSize, nextValue: () -> CGSize) {
+    value = nextValue()
+  }
+}
+
+struct ContentSizeKey: PreferenceKey {
+  static var defaultValue: CGSize { .zero }
+  static func reduce(value: inout CGSize, nextValue: () -> CGSize) {
+    value = nextValue()
+  }
+}
