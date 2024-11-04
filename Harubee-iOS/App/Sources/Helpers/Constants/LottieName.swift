c1f2dbba48bf9abc35d5543967775e911aae3ca7
Jeong Dong <eejxdong99@gmail.com>
2024-11-04 22:12:43 +0900

    Feature/#99: Lottie 리소스 적용

---
+++ Harubee-iOS/App/Sources/Helpers/Constants/LottieName.swift
@@ -0,0 +1,15 @@
+//
+//  LottieName.swift
+//  Harubee-iOS
+//
+//  Created by 이정동 on 11/4/24.
+//  Copyright © 2024 namudiEtc. All rights reserved.
+//
+
+import Foundation
+
+struct LottieName {
+  static let onboarding = "onboarding"
+}
+
+
