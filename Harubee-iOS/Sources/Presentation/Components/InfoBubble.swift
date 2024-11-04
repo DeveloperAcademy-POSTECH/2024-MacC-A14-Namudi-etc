1dcea5208c70d773daf174ccb024d2b8e714038b
shinseungjae <mok05247@naver.com>
2024-11-04 20:08:06 +0900

    Feature/#101: 코멘트 수정사항 반영

--- Harubee-iOS/Sources/Presentation/Components/InfoBubble.swift
+++ Harubee-iOS/Sources/Presentation/Components/InfoBubble.swift
@@ -14,20 +14,17 @@ struct InfoBubble<Label: View>: ViewModifier {
   @State private var contentSize: CGSize = .zero
   @State private var labelSize: CGSize = .zero
   
-  private var isVisible: Bool
+  @Binding private var isVisible: Bool
   private var alignment: Alignment
-  private var isOnBottom: Bool
   private var label: () -> Label
   
-  private let spacing = 30.0
+  private let spacing:CGFloat = 15.0
   
-  init(isVisible: Bool,
+  init(isVisible: Binding<Bool>,
        alignment: Alignment,
-       isOnBottom: Bool,
        @ViewBuilder label: @escaping () -> Label) {
-    self.isVisible = isVisible
+    self._isVisible = isVisible
     self.alignment = alignment
-    self.isOnBottom = isOnBottom
     self.label = label
   }
   
@@ -60,46 +57,52 @@ struct InfoBubble<Label: View>: ViewModifier {
                 labelSize = newSize
               }
           }
-          .background(Color.whiteDefault)
-          .cornerRadius(8)
-          .offset(calculateOffset())
+            .background(Color.whiteDefault)
+            .cornerRadius(8)
+            .offset(calculateOffset())
         )
         .overlay(
           Image(systemName: "triangle.fill")
             .resizable()
             .frame(width: 20, height: 14)
-            .rotationEffect(.degrees(isOnBottom ? 0 : 180))
+            .rotationEffect(.degrees([.bottom, .bottomLeading, .bottomTrailing].contains(alignment) ? 0 : 180))
             .foregroundStyle(Color.whiteDefault)
-            .offset(y: isOnBottom ? (contentSize.height + spacing) / 2 : -(contentSize.height + spacing) / 2)
+            .offset(y: [.bottom, .bottomLeading, .bottomTrailing].contains(alignment) ? contentSize.height / 2 + spacing : -contentSize.height / 2 - spacing)
         )
     } else { content }
   }
   
   private func calculateOffset() -> CGSize {
-    let xOffset: CGFloat
-    let yOffset: CGFloat
+    let xOffset: CGFloat = {
+      switch alignment {
+      case .topLeading, .bottomLeading:
+        return -(contentSize.width - labelSize.width) / 2
+      case .topTrailing, .bottomTrailing:
+        return (contentSize.width - labelSize.width) / 2
+      default:
+        return 0
+      }
+    }()
     
-    switch alignment {
-    case .leading:
-      xOffset = -(contentSize.width - labelSize.width) / 2
-    case .trailing:
-      xOffset = (contentSize.width - labelSize.width) / 2
-    default:
-      xOffset = 0
-    }
-    
-    yOffset = isOnBottom ? (contentSize.height + labelSize.height + spacing) / 2 : -(labelSize.height + contentSize.height + spacing) / 2
+    let yOffset: CGFloat = {
+      switch alignment {
+      case .bottom, .bottomLeading, .bottomTrailing:
+        return (contentSize.height + labelSize.height) / 2 + spacing
+      default:
+        return -(labelSize.height + contentSize.height) / 2 - spacing
+      }
+    }()
     
     return CGSize(width: xOffset, height: yOffset)
   }
 }
 
+
 extension View {
-  public func infoBubble<Label: View>(isVisible: Bool,
+  public func infoBubble<Label: View>(isVisible: Binding<Bool>,
                                       alignment: Alignment = .center,
-                                      isOnBottom: Bool = false,
                                       @ViewBuilder label: @escaping () -> Label) -> some View {
-    modifier(InfoBubble(isVisible: isVisible, alignment: alignment, isOnBottom: isOnBottom, label: label))
+    modifier(InfoBubble(isVisible: isVisible, alignment: alignment, label: label))
   }
 }
 
