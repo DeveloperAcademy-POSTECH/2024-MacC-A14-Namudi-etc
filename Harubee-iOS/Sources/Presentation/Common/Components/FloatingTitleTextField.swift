309d7916fdf27a1485467ee4b83a5f55e8c920a7
Jeong Dong <eejxdong99@gmail.com>
2024-11-15 00:07:33 +0900

    Feature/#142: 하루비 조정 화면 일부 수정

--- Harubee-iOS/Sources/Presentation/Common/Components/FloatingTitleTextField.swift
+++ Harubee-iOS/Sources/Presentation/Common/Components/FloatingTitleTextField.swift
@@ -47,3 +47,55 @@ struct FloatingTitleTextField: View {
     }
   }
 }
+
+struct FloatingTitleNumberField: View {
+  
+  let title: String
+  @Binding var text: String
+  @Binding var isFocused: Bool
+  
+  var body: some View {
+    VStack(alignment: .leading, spacing: 0) {
+      Text(title)
+        .font(.pretendardMedium_12)
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .foregroundStyle(!text.isEmpty ? Color.main : .clear)
+        .offset(y: !text.isEmpty ? -2 : 0)
+        .animation(.easeOut(duration: 0.2), value: !text.isEmpty)
+        .padding(.leading, 4)
+      
+      Text(text.isEmpty ? title : text)
+        .foregroundStyle(!text.isEmpty ? Color.textBlack : Color.textBrighter)
+        .font(.pretendardSemibold_28)
+        .padding(.leading, 4)
+      
+      Rectangle()
+        .frame(height: 1)
+        .foregroundStyle(
+          (!text.isEmpty || isFocused) ? Color.main : Color.textBrighter
+        )
+        .padding(.top, 8)
+    }
+    .frame(maxWidth: .infinity)
+  }
+}
+
+#Preview {
+  @Previewable @State var text = ""
+  @Previewable @State var isFocused = false
+  
+  VStack {
+    Spacer()
+    FloatingTitleTextField(title: "Title", text: $text)
+    FloatingTitleNumberField(title: "Test", text: $text, isFocused: $isFocused)
+      .onTapGesture {
+        isFocused = true
+      }
+    Spacer()
+  }
+  .contentShape(Rectangle())
+  .onTapGesture {
+    isFocused = false
+  }
+  
+}
