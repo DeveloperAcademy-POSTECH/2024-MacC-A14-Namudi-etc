6ef0108ddc476764b4bd363b8ce75f80a15c1091
Jeong Dong <eejxdong99@gmail.com>
2024-11-18 15:17:43 +0900

    Feature/#142: 고정수입 수정 화면 수정

--- Harubee-iOS/Sources/Presentation/Setting/Views/FixedIncomeModifyView.swift
+++ Harubee-iOS/Sources/Presentation/Setting/Views/FixedIncomeModifyView.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct FixedIncomeModifyView: View {
   @Environment(\.dismiss) private var dismiss
   @State private var fixedIncomeAmount: String
+  @State private var isFocused: Bool = false
   
   private var action: (String) -> Void
   
@@ -18,36 +19,41 @@ struct FixedIncomeModifyView: View {
     fixedIncomeAmount: String,
     action: @escaping (String) -> Void
   ) {
-    self.fixedIncomeAmount = fixedIncomeAmount
+    self._fixedIncomeAmount = State(initialValue: fixedIncomeAmount)
     self.action = action
   }
   
   var body: some View {
-    VStack(spacing: 0) {
-      Text("금액")
-        .font(.pretendardMedium_18)
-        .foregroundStyle(Color.textBlack)
-        .padding(.top, 20)
-      
-      FloatingTitleTextField(title: "금액", text: $fixedIncomeAmount)
-        .disabled(true)
-        .padding(.top, 38)
-        .padding(.horizontal, 16)
-      
-      MainColorBottomButton(
-        title: "완료하기",
-        isEnabled: .constant(true)
-      ) {
-        action(fixedIncomeAmount)
-        dismiss()
+    ZStack(alignment: .bottom) {
+      VStack(spacing: 0) {
+        BottomSheetHeaderView(title: "고정수입 금액 입력")
+        
+        FloatingTitleTextField(title: "금액", text: $fixedIncomeAmount)
+          .disabled(true)
+          .padding(.top, 38)
+          .padding(.horizontal, 16)
+          .contentShape(Rectangle())
+          .onTapGesture {
+            isFocused = true
+          }
+        
+        Spacer()
+        
+        MainColorBottomButton(
+          title: "완료하기",
+          isEnabled: .constant(true)
+        ) {
+          action(fixedIncomeAmount)
+          dismiss()
+        }
       }
-      .padding(.top, 24)
       
-//      NumberKeypadView(expression: $fixedIncomeAmount) { isEnabled in
-//        print("Done")
-//      }
+      if isFocused {
+        NumberKeypadView(amount: $fixedIncomeAmount) {
+          isFocused = false
+        }
+      }
     }
-    .ignoresSafeArea()
   }
 }
 
