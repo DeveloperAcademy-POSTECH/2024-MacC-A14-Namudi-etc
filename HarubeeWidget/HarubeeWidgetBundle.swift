5c77e7a2cd0083b42233556497f4d17865e0b462
Jeong Dong <eejxdong99@gmail.com>
2024-11-20 21:42:42 +0900

    Chore/#153: WidgetExtension, AppGroup 추가

---
+++ HarubeeWidget/HarubeeWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  HarubeeWidgetBundle.swift
+//  HarubeeWidget
+//
+//  Created by 이정동 on 11/20/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct HarubeeWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        HarubeeWidget()
+    }
+}
