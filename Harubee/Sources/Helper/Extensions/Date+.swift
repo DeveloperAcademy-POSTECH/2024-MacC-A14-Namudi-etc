b26c4bf058799c13b28ab6e4b125310797dd1ebd
Jeong Dong <eejxdong99@gmail.com>
2024-12-01 01:25:49 +0900

    Hotfix/#180: 새로운 월급기간에 호출되는 SalaryBudget 생성 로직 수정

--- Harubee/Sources/Helper/Extensions/Date+.swift
+++ Harubee/Sources/Helper/Extensions/Date+.swift
@@ -70,6 +70,8 @@ extension Date {
     return formatter.string(from: self)
   }
   
+  
+  // TODO: 수정 필요
   /// 수입일을 기준으로 이번 월급 기간을 계산해줍니다.
   /// - Parameter incomeDay: 수입일
   /// - Returns: 이번 월급 기간의 시작 및 종료 날짜
--- Harubee/Sources/Helper/Extensions/Int+.swift
+++ Harubee/Sources/Helper/Extensions/Int+.swift
@@ -45,20 +45,25 @@ extension Int {
   func convertDateBetweenStartAndEnd(start: Date, end: Date) -> Date {
     let calendar = Calendar.current
     
-    var startDateComponents = calendar.dateComponents([.year, .month, .day], from: start)
-    let endDateComponents = calendar.dateComponents([.year, .month, .day], from: end)
+    var current = start
     
-    while startDateComponents != endDateComponents {
-      let day = startDateComponents.day!
-      if day == self {
-        return calendar.date(from: startDateComponents)!
-      }
-      startDateComponents.day! += 1
+    while current <= end {
+      let day = current.day
       
-      let start = calendar.date(from: startDateComponents)!
-      startDateComponents = calendar.dateComponents([.year, .month, .day], from: start)
+      if day == self { return current }
+      
+      current.addTimeInterval(86400)
     }
     
-    return calendar.date(from: endDateComponents)!
+    // 해당 날짜가 기간 사이에 존재하지 않는 경우는 시작 날짜의 달의 마지막 날을 리턴
+    let dateComponents = calendar.dateComponents([.year, .month], from: start)
+    let date = calendar.date(from: dateComponents)!
+    let lastDay = calendar.range(of: .day, in: .month, for: date)!.last!
+    
+    return Date.create(
+      year: dateComponents.year!,
+      month: dateComponents.month!,
+      day: lastDay
+    )
   }
 }
