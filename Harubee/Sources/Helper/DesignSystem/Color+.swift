84fe937e2a76490427de122688e8a1266fb287a6
shinseungjae <mok05247@naver.com>
2025-01-10 12:57:38 +0900

    Feature/#237: FillStyle extension에 새로운 컬러 추가

--- Harubee/Sources/Helper/DesignSystem/Color+.swift
+++ Harubee/Sources/Helper/DesignSystem/Color+.swift
@@ -9,6 +9,7 @@
 import Foundation
 import SwiftUI
 
+// Legacy
 extension FillStyle {
   static let main = Color(.main)
   static let main10 = Color(.main10)
@@ -37,3 +38,60 @@ extension FillStyle {
   
   static let placeholder = Color(.placeholder)
 }
+
+// New
+extension FillStyle {
+  static let alertBg = Color(.alertBg)
+  static let alertText = Color(.alertText)
+  
+  static let bgAccent = Color(.bgAccent)
+  static let bgPrimary = Color(.bgPrimary)
+  static let bgSecondary = Color(.bgSecondary)
+  static let bgSecondary50 = Color(.bgSecondary50)
+  
+  static let dayText = Color(.dayText)
+  static let hivePrimary = Color(.hivePrimary)
+  static let hivePrimaryBack = Color(.hivePrimaryBack)
+  static let hivePrimaryButton = Color(.hivePrimaryButton)
+  
+  static let hiveSecondary = Color(.hiveSecondary)
+  static let hiveSecondaryBack = Color(.hiveSecondaryBack)
+  static let hiveSecondaryText = Color(.hiveSecondaryText)
+  static let hiveSecondaryButton = Color(.hiveSecondaryButton)
+  
+  static let hiveText = Color(.hiveText)
+  static let info = Color(.info)
+  
+  static let mainBgAccent = Color(.mainBgAccent)
+  static let mainBgPrimary = Color(.mainBgPrimary)
+  
+  static let mainPrimary = Color(.mainPrimary)
+  static let mainPrimary10 = Color(.mainPrimary10)
+  static let mainPrimary30 = Color(.mainPrimary30)
+  static let mainSecondary = Color(.mainSecondary)
+  
+  static let mainTertiary10 = Color(.mainTertiary10)
+  static let mainTertiary15 = Color(.mainTertiary15)
+  static let mainTertiary60 = Color(.mainTertiary60)
+  
+  static let mainText = Color(.mainText)
+  static let strict = Color(.strict)
+  
+  static let textFixed = Color(.textFixed)
+  static let textPrimary = Color(.textPrimary)
+  static let textPrimary5 = Color(.textPrimary5)
+  static let textPrimary10 = Color(.textPrimary10)
+  static let textPrimary30 = Color(.textPrimary30)
+  
+  static let textSecondary = Color(.textSecondary)
+  static let textSecondary40 = Color(.textSecondary40)
+  static let textSecondaryInversion = Color(.textSecondaryInversion)
+  static let textTertiary = Color(.textTertiary)
+  static let textTertiary30 = Color(.textTertiary30)
+  
+  static let warning = Color(.warning)
+  static let warning10 = Color(.warning10)
+}
+
+
+
