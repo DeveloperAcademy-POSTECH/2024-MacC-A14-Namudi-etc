3bc3af7b3a0168d34e94c90d28ad953fc371806c
shinseungjae <mok05247@naver.com>
2025-01-13 17:55:17 +0900

    Feature/#237: 설정 뷰 다크모드 적용

--- Harubee/Sources/Presentation/Common/Components/TimePickerView.swift
+++ Harubee/Sources/Presentation/Common/Components/TimePickerView.swift
@@ -20,7 +20,7 @@ struct TimePickerView: View {
         
         Text(title)
           .font(.pretendardSemibold_18)
-          .foregroundStyle(Color.textBlack)
+          .foregroundStyle(.textPrimary)
         
         Spacer()
         
@@ -33,13 +33,13 @@ struct TimePickerView: View {
         Toggle("", isOn: $isToggleOn)
           .frame(width: 51, height: 31)
           .padding(.leading, 14)
-          .tint(.main)
+          .toggleStyle(CustomToggleStyle())
       }
 
       if showPicker {
         Rectangle()
           .frame(height: 1)
-          .foregroundStyle(Color.textBrighter30)
+          .foregroundStyle(.textTertiary30)
           .padding(.top, 14)
           .padding(.horizontal, 16)
   
@@ -79,15 +79,41 @@ private struct PickerButton: View {
       Text(selectedTime.formattedDateToString(.time_kr))
         .font(.pretendardMedium_16)
         .foregroundStyle(
-          isToggleOn ? (showPicker ? Color.main : Color.textBlack)
-                     : .textBlack30
+          isToggleOn ? (showPicker ? .mainPrimary : .textPrimary)
+                     : .textTertiary
         )
         .padding(.vertical, 6)
         .padding(.horizontal, 11)
         .background(
           RoundedRectangle(cornerRadius: 6)
-            .foregroundStyle(Color.textBrighter30)
+            .foregroundStyle(Color.textTertiary30)
         )
     }.disabled(!isToggleOn)
+  }
+}
+
+struct CustomToggleStyle: ToggleStyle {
+  var onColor: Color = .mainPrimary
+  var offColor: Color = .textPrimary10
+  var thumbColor: Color = .bgPrimary
+  
+  func makeBody(configuration: Self.Configuration) -> some View {
+    HStack {
+      Spacer()
+      Button(action: { configuration.isOn.toggle() } )
+      {
+        RoundedRectangle(cornerRadius: 16, style: .circular)
+          .fill(configuration.isOn ? onColor : offColor)
+          .frame(width: 50, height: 29)
+          .overlay(
+            Circle()
+              .fill(thumbColor)
+              .shadow(radius: 1, x: 0, y: 1)
+              .padding(1.5)
+              .offset(x: configuration.isOn ? 10 : -10))
+          .animation(.easeInOut, value: 0.1)
+      }
+    }
+    .padding(.horizontal)
   }
 }
