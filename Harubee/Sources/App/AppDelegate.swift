73ca53961134c7acfed3b784b01ed7090fa61768
namudi <namudongs@gmail.com>
2025-01-19 05:17:21 +0900

    Feature/#236: Scheme 이름 변경

--- Harubee/Sources/App/AppDelegate.swift
+++ Harubee/Sources/App/AppDelegate.swift
@@ -12,16 +12,15 @@ class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
     
-#if DEBUG
-    // Debug 환경의 GoogleService-Info.plist 설정
+#if DEV
+    // DEV 환경의 GoogleService-Info.plist 설정
     guard let filePath = Bundle.main.path(forResource: "GoogleService-Info-Dev", ofType: "plist"),
           let options = FirebaseOptions(contentsOfFile: filePath)
     else { return false }
     
     FirebaseApp.configure(options: options)
-#else
-    print("is RELEASE")
-    // Release 환경의 GoogleService-Info.plist 설정
+#elseif PROD
+    // PROD 환경의 GoogleService-Info.plist 설정
     guard let filePath = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist"),
           let options = FirebaseOptions(contentsOfFile: filePath)
     else { return false }
